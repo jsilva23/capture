dd4fb3604e4119aa9fbde4b67ed4e471a411cf1f
José da Silva <josesilva23p@gmai..com>
2021-05-26 20:07:03 +0100

    3. Services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,11 @@
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection"
 
 const AboutUs = () => {
   return (
     <di>
       <AboutSection />
+      <ServicesSection />
     </di>
   );
 };
