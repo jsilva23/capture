e629c2650fe2e3eb8da8e2a1dc55220a15eff596
José da Silva <josesilva23p@gmai..com>
2021-05-28 14:56:43 +0100

    9. Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 // Global style
 import GlobalStyle from "./components/GlobalStyle";
+//nav component
 import Nav from "./components/Nav";
 //Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// routes
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+
+        <Route path="/work">
+          <OurWork />
+        </Route>
+
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
